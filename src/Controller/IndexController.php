12911b8b0c76b9f551090d0e218a807194c71bbf
EndePointe <alvin.johnsii@gmail.com>
2019-07-05 12:54:11 -0700

    morelearned

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,24 +1,37 @@
 <?php
-// src/Controller/TestController.php
+// src/Controller/IndexController.php
 
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController
 {
+	/**
+   * @Route("/")
+	*/
 
-	public function test()
+	public function loadIndex()
 	{
 
-		$number = random_int(0,99);
+		$greeting = '<h1>Welcome to the landing page</h1>';
 
 		return new Response(
 			'<html>
 				<body>
-					Test number is:'.$number.'
+					'.$greeting.'
 				</body>
 			</html>');
+	}
+
+	/**
+		* @Route("/login")
+	*/
+	public function  login()
+	{
+		$login_form = "tHIS will Be A FOrm";
+		return new Response($login_form);
 	}
 }
 ?>
