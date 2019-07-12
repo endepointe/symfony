0ab0174dcdfb32fd0ec5957c07906f3d6ead41de
EndePointe <alvin.johnsii@gmail.com>
2019-07-12 08:06:31 -0700

    removed blanks

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-<<<<<<< HEAD
 class TaskType extends AbstractType
 {
 	public function buildForm(FormBuilderInerface $builder, array $options)
@@ -21,7 +20,6 @@ class TaskType
 	public function doSomething()
 	{
 		// ... something gets done
->>>>>>> 2969f5309e2fc7531181ce87ddfcfe51afc34084
 	}
 }
 ?>
