1db2b103fe0a533b859064a41ba34338de0cdfc1
EndePointe <alvin.johnsii@gmail.com>
2019-07-15 21:08:42 -0700

    progress made, short brk

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
 // assets/js/app.js
 
-import React from 'react';
-import Test from './test';
+import React from "react";
+import ReactDOM from "react-dom";
+import Test from "./test";
+
+class Welcome extends React.Component {
+  render() {
+    return <h1>Welcome, {this.props.name}</h1>
+  }
+}
 
 (() => {
-    console.log(Test(`Test from app.js\nReact Object:`), React);
-})();
+  const welcome = <Welcome name="BuildA"></Welcome>
+  ReactDOM.render(welcome, document.getElementById('root'));
+  console.log(Test(`Test from app.js\nReact Object:`), React)
+})()
