f862ca611f247cca50bc8ababde0174cb75cbcd3
EndePointe <alvin.johnsii@gmail.com>
2019-07-16 15:56:08 -0700

    resolved console ghost error

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,9 @@
 // assets/js/app.js
-"use strict"
+//"use strict"
 
-import React from "react"
-import ReactDOM from "react-dom"
-import Test from "./test" // semi colons causing an error, but why?
-
-class Hello extends React.Component {
-  render() {
-    return <h1>Hi, {this.props.name}{this.message}</h1>
-  }
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import Test from "./test";
 
 class App extends React.Component {
   render() {
