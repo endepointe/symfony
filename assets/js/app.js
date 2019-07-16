3b3416224f77497a8d3d8a1b673f4d9763608316
EndePointe <alvin.johnsii@gmail.com>
2019-07-16 14:51:04 -0700

    switched devices

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,6 @@ import Test from "./test" // semi colons causing an error, but why?
 
 class Hello extends React.Component {
   render() {
-<<<<<<< HEAD
-    return <h1>Welcome, {this.props.name}</h1>;
-=======
     return <h1>Hi, {this.props.name}{this.message}</h1>
   }
 }
@@ -57,19 +54,11 @@ class Clock extends React.Component {
         <h1>Hello, world!</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
-    );
->>>>>>> 011b700696163463e4aafbbaf7f9f5e45777af80
+    )
   }
 }
 
 (() => {
-<<<<<<< HEAD
-  const welcome = <Hello name="X-name inserted from Hello class-X" />
-  ReactDOM.render(welcome, document.getElementById('root'));
-  console.log(Test(`Test from app.js\nReact Object:`), React)
-})()
-=======
   ReactDOM.render(<App />, document.getElementById("root"))
   console.log(Test(`Test from app.js\n\nReact Object:`), React)
 })()
->>>>>>> 011b700696163463e4aafbbaf7f9f5e45777af80
